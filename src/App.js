15395d7729b08263854f55304cf5b56cd40d8e49
Griv G <isaacjmadden@gmail.com>
2020-01-05 22:55:48 -0500

    first commitX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ColorPicker from 'react-color-picker'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={ color: 'red'}
+  }
+
+  onDrag(color, c) {
+    this.setState({color})
+  }
+
+  render() {
+    return <div>
+      <ColorPicker value={this.state.color} onDrag={this.onDrag.bind(this)} />
+      <div style={{
+        background: this.state.color,
+        width: 100,
+        height: 50,
+        color: 'white'
+      }}>
+        {this.state.color}
+        </div>
     </div>
-  );
+  }
 }
 
 export default App;
